cc3ff96d4923fb900c35ca5176eb428f37aa3e2b
agustinvd10 <avalenzu@chicagobooth.edu>
2021-04-20 20:20:57 +0000

    added

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :comments
-  # has_many :likes
-  # has_many :photos
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :fan_id
+  has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+
 end
