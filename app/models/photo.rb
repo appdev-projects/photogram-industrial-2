8c63a37bbd6d4bc016982a9cfccc3812c6b83614
yanyuliu92 <yliu33@chicagobooth.edu>
2021-04-21 03:49:26 +0000

    Added validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -25,4 +25,7 @@ class Photo < ApplicationRecord
   has_many :comments
   has_many :likes, foreign_key: :fan_id
   has_many :fans, through: :likes
+
+  validates :caption, presence: true
+  validates :image, presence: true
 end
