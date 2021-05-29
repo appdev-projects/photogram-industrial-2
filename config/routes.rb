ab24e9328116af70e62b01b3d3951218f113d518
Narek-booth <narekmail@mail.ru>
2021-05-29 23:10:23 +0000

    added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+
+  #get "/users/:id" => "users#show", as: :user
+  
+  
+  
   devise_for :users
 
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get "/:username" => "users#show"
 end
