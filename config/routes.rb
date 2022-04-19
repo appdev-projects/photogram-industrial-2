5aefe2ff47af3f30b0a779a69f0b0d049f8b89a3
RilezLed <rledezma@chicagobooth.edu>
2022-04-19 11:17:00 +0000

    Last Few Things

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,22 @@ Rails.application.routes.draw do
 
   #get "/user/:id" => "users#show", as: :user:
   
-  
+  devise_for :users
 
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  resources :users, only: :show
-  devise_for :users
+  #resources :users, only: :show
+  
 
-  get "/:username" => "users#show"
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  #get ":username/feed" => "photos#feed", as: :liked_photos
+
+  #get ":username/followers" => "photos#followers", as: :followers_photos
+  #get ":username/follwing" => "photos#follwing", as: :following_photos
+
+
+  get ":username" => "users#show", as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
