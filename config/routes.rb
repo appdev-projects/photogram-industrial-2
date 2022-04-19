025618e45489cb4532ebca162919f7bb89d7a9ae
RilezLed <rledezma@chicagobooth.edu>
2022-04-19 11:55:55 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
   
 
   get ":username/liked" => "photos#liked", as: :liked_photos
-  #get ":username/feed" => "photos#feed", as: :liked_photos
+  get ":username/feed" => "photos#feed", as: :feed_photos
 
-  #get ":username/followers" => "photos#followers", as: :followers_photos
-  #get ":username/follwing" => "photos#follwing", as: :following_photos
+  get ":username/followers" => "photos#followers", as: :followers_photos
+  get ":username/following" => "photos#following", as: :following_photos
 
 
-  get ":username" => "users#show", as: :user
+  get "/:username" => "users#show", as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
