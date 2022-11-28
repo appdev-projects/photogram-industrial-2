03303da41677d91bb04da81ea28784d41b57ebb3
DiegoCruz02 <a.diegocruz02@gmail.com>
2022-11-28 19:18:45 +0000

    Added comment model logic

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   # cool... because we used refrences we had an association already defined for us once we migrated the code
   # remember if we go agianst convention we need to include class_name: "User" ... we chose ot call our foreign key owner instead of user
   belongs_to :owner, class_name: "User"
+
+  has_many :comments
 end
