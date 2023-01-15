b42312c1c69971e74155772f76a4c873b3c28332
JackRB52 <jbouchard@chicagobooth.edu>
2023-01-15 02:05:26 +0000

    added auto annotate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  author_id  :bigint           not null
+#  photo_id   :bigint           not null
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Comment < ApplicationRecord
   validates :commenter, presence: true
   validates :photo, presence: true
