8cda9b04f7595802ec57d51be847f68df9764ec5
Justin1111111111 <stuwrkr-jguzm21@mx.uillinois.edu>
2023-05-06 03:23:19 +0000

    set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+
   devise_for :users
 
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
+  resources :users, only: :show
 
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
