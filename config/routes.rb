08b2212c4c10e8fcf3ba2cff40ef70f477c8ae77
Justin1111111111 <stuwrkr-jguzm21@mx.uillinois.edu>
2023-05-06 00:07:11 +0000

    Added alerts and flash

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
 
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
