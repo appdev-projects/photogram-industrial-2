b86816c9f058e7a1d667a5a927f047e7815e38b8
John Leavell <johnleavell@gmail.com>
2023-06-22 13:40:11 -0500

    added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+
   devise_for :users
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  resources :users, only: :show
 
+
+  get "/:username" => "users#show"
 
 end
